154242b54ef82b767c9ca0480b9ddfa6ac421f32
jhfhngj <ezzeldinahmed1@outlook.com>
2025-01-25 13:35:06 -0600

    add if = statement

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def splitter(text: str):
     return text.split("(")
 var = {}
 libs = []
+out = ""
 def read(code):
+    out = ""
     for line in code:
         if splitter(line)[0] == "prt":
             if var.get(splitter(line)[1]) == None:
@@ -21,8 +23,38 @@ def read(code):
                 with open(splitter(line)[1]) as f:
                     r = f.readlines()
                 read(r)
+        elif splitter(line)[0] == "if=":
+            if var.get(splitter(line)[1]) == None:
+                v1 = "str"
+            else:
+                v1 = "var"
+            if var.get(splitter(line)[2]) == None:
+                v2 = "str"
+            else:
+                v2 = "var"
+            if v1 == "str" and v2 == "str":
+                if splitter(line)[1] == splitter(line)[2]:
+                    out = "True"
+                else:
+                    out = "False"
+            if v1 == "var" and v2 == "var":
+                if var.get(splitter(line)[1]) == var.get(splitter(line)[2]):
+                    out = "True"
+                else:
+                    out = "False"
+            if v1 == "str" and v2 == "var":
+                if splitter(line)[1] == var.get(splitter(line)[1]):
+                    out = "True"
+                else:
+                    out = "False"
+            if v1 == "var" and v2 == "str":
+                if var.get(splitter(line)[1]) == splitter(line)[2]:
+                    out = "True"
+                else:
+                    out = "False"
+            var.update({splitter(line)[3]: out})
+           
 def rfl(file):
     with open(file,"r") as f:
         reads = f.readlines()
     read(reads)
-    
