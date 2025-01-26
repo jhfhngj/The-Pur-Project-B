4529c09efc749ec050837ae2ffb4466e82014894
jhfhngj <ezzeldinahmed1@outlook.com>
2025-01-26 10:44:49 -0600

    I don't really know anymore what names I should do

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 import sys
-def splitter(text: str):
+def s(text: str):
     return text.split("(")
 var = {}
 libs = []
@@ -17,94 +17,94 @@ def read(code):
             end = code.index("end(", i)
         except:
             end = len(code)
-        if splitter(line)[0] == "prt":
-            if var.get(splitter(line)[1]) == None:
-                print(splitter(line)[1])
+        if s(line)[0] == "prt":
+            if var.get(s(line)[1]) == None:
+                print(s(line)[1])
             else:
-                print(var.get(splitter(line)[1]))
-        elif splitter(line)[0] == "into":
+                print(var.get(s(line)[1]))
+        elif s(line)[0] == "into":
             do = input()
-            var.update({str(splitter(line)[1]): do})
-        elif splitter(line)[0] == "get":
-            if os.path.exists(splitter(line)[1]):
-                libs.append(splitter(line)[1])
-        elif splitter(line)[0] == "runlib":
-            if libs.count(splitter(line)[1]) > 0:
-                with open(splitter(line)[1]) as f:
+            var.update({str(s(line)[1]): do})
+        elif s(line)[0] == "get":
+            if os.path.exists(s(line)[1]):
+                libs.append(s(line)[1])
+        elif s(line)[0] == "runlib":
+            if libs.count(s(line)[1]) > 0:
+                with open(s(line)[1]) as f:
                     r = f.readlines()
                 read(r)
-        elif splitter(line)[0] == "if!=":
-            if var.get(splitter(line)[1]) == None:
+        elif s(line)[0] == "if!=":
+            if var.get(s(line)[1]) == None:
                 v1 = "str"
             else:
                 v1 = "var"
-            if var.get(splitter(line)[2]) == None:
+            if var.get(s(line)[2]) == None:
                 v2 = "str"
             else:
                 v2 = "var"
             if v1 == "str" and v2 == "str":
-                if splitter(line)[1] != splitter(line)[2]:
+                if s(line)[1] != s(line)[2]:
                     out = "True"
                 else:
                     out = "False"
             if v1 == "var" and v2 == "var":
-                if var.get(splitter(line)[1]) != var.get(splitter(line)[2]):
+                if var.get(s(line)[1]) != var.get(s(line)[2]):
                     out = "True"
                 else:
                     out = "False"
             if v1 == "str" and v2 == "var":
-                if splitter(line)[1] != var.get(splitter(line)[1]):
+                if s(line)[1] != var.get(s(line)[1]):
                     out = "True"
                 else:
                     out = "False"
             if v1 == "var" and v2 == "str":
-                if var.get(splitter(line)[1]) != splitter(line)[2]:
+                if var.get(s(line)[1]) != s(line)[2]:
                     out = "True"
                 else:
                     out = "False"
-            var.update({splitter(line)[3]: out})
+            var.update({s(line)[3]: out})
             if out == "False":
                 i = end
-        elif splitter(line)[0] == "if=":
-            if var.get(splitter(line)[1]) == None:
+        elif s(line)[0] == "if=":
+            if var.get(s(line)[1]) == None:
                 v1 = "str"
             else:
                 v1 = "var"
-            if var.get(splitter(line)[2]) == None:
+            if var.get(s(line)[2]) == None:
                 v2 = "str"
             else:
                 v2 = "var"
             if v1 == "str" and v2 == "str":
-                if splitter(line)[1] == splitter(line)[2]:
+                if s(line)[1] == s(line)[2]:
                     out = "True"
                 else:
                     out = "False"
             if v1 == "var" and v2 == "var":
-                if var.get(splitter(line)[1]) == var.get(splitter(line)[2]):
+                if var.get(s(line)[1]) == var.get(s(line)[2]):
                     out = "True"
                 else:
                     out = "False"
             if v1 == "str" and v2 == "var":
-                if splitter(line)[1] == var.get(splitter(line)[1]):
+                if s(line)[1] == var.get(s(line)[1]):
                     out = "True"
                 else:
                     out = "False"
             if v1 == "var" and v2 == "str":
-                if var.get(splitter(line)[1]) == splitter(line)[2]:
+                if var.get(s(line)[1]) == s(line)[2]:
                     out = "True"
                 else:
                     out = "False"
-            var.update({splitter(line)[3]: out})
+            var.update({s(line)[3]: out})
             if out == "False":
                 i = end
-        elif splitter(line)[0] == "frvr":
+        elif s(line)[0] == "frvr":
             start = i
             typel = "loopf"
-        # elif splitter(line)[0] == "rpt":
+        # elif s(line)[0] == "rpt":
         #     start = i
         #     typel = "loopr"
-        #     times = splitter(line)[1]
-        elif splitter(line)[0] == "end":
+        #     times = s(line)[1]
+        elif s(line)[0] == "end":
             end = i
             if typel == 'loopf':
                 i = start
